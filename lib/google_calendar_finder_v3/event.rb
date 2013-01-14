e0d49241a267fd4371a14e5e897c83cfca5543bf
Pablo <bianciottopablo@gmail.com>
2013-01-14 16:59:32 -0300

    to be amended

--- lib/google_calendar_finder_v3/event.rb
+++ lib/google_calendar_finder_v3/event.rb
@@ -6,21 +6,55 @@ module GCFinder
     MUST_HAVE_FIELDS = 2
 
     class << self
+      def get(authorization, calendar_id, event_id)
+        result = api_client.execute(
+          api_method: service.events.get,
+          parameters: {'calendarId' => calendar_id, 'eventId' => event_id},
+          authorization: authorization)
+        JSON.parse(result.body)
+      end
+
+      def update_or_create!(authorization, calendar_id, event_metadata)
+        if (event_metadata.has_key?(:id) && event = get(authorization, calendar_id, event_metadata[:id]))
+          p 'está, updateémoslo!'
+          p "#{event}"
+          update!(authorization, calendar_id, event_id, event.merge(event_metadata.except(:id)))
+        else
+          p 'no está, creémoslo!'
+          create!(authorization, calendar_id, event_metadata)
+        end
+      end
+
+      def create!(authorization, calendar_id, event_metadata)
+        event_metadata.stringify_keys!
+        result = GCFinder.api_client.execute(
+          api_method: GCFinder.google_calendar.events.insert,
+          parameters: {'calendarId' => calendar_id},
+          body: JSON.dump(event_metadata),
+          headers: {'Content-Type' => 'application/json'},
+          authorization: authorization)
+        JSON.parse(result.body)
+      end
+
+      def update!(authorization, calendar_id, event_id, event_metadata)
+        p 'ENTRE EN UPDATE'
+      end
+
       def find(authorization, calendar_id, &keep_if_block)
         puts "#{name}: Ejecutando find en #{calendar_id} #{(block_given?)? 'CON' : 'SIN'} bloque"
         result = GCFinder.api_client.execute(
-            api_method: GCFinder.google_calendar.events.list,
-            parameters: {'calendarId' => calendar_id},
-            authorization: authorization)
+          api_method: GCFinder.google_calendar.events.list,
+          parameters: {'calendarId' => calendar_id},
+          authorization: authorization)
         while true
           hashed_body = JSON.parse(result.body)
           match = hashed_body['items'].find(&keep_if_block)
           return match if match
           return nil if !(page_token = result.data.next_page_token)
           result = api_client.execute(
-              api_method: GCFinder.google_calendar.events.list,
-              parameters: {'calendarId' => calendar_id, 'pageToken' => page_token},
-              authorization: authorization)
+            api_method: GCFinder.google_calendar.events.list,
+            parameters: {'calendarId' => calendar_id, 'pageToken' => page_token},
+            authorization: authorization)
         end
       end
 
@@ -28,9 +62,9 @@ module GCFinder
         puts "#{name}: Ejecutando select en #{calendar_id} #{(block_given?)? 'CON' : 'SIN'} bloque"
         matches = []
         result = GCFinder.api_client.execute(
-            api_method: GCFinder.google_calendar.events.list,
-            parameters: {'calendarId' => calendar_id},
-            authorization: authorization)
+          api_method: GCFinder.google_calendar.events.list,
+          parameters: {'calendarId' => calendar_id},
+          authorization: authorization)
         while true
           hashed_body = JSON.parse(result.body)
           selected_items = if block_given?
@@ -41,9 +75,9 @@ module GCFinder
           matches.concat(selected_items)
           return matches.compact if !(page_token = result.data.next_page_token)
           result = api_client.execute(
-              api_method: GCFinder.google_calendar.events.list,
-              parameters: {'calendarId' => calendar_id, 'pageToken' => page_token},
-              authorization: authorization)
+            api_method: GCFinder.google_calendar.events.list,
+            parameters: {'calendarId' => calendar_id, 'pageToken' => page_token},
+            authorization: authorization)
         end
       end
 
@@ -52,17 +86,17 @@ module GCFinder
         puts "#{name}: Ejecutando reject en #{calendar_id} #{(block_given?)? 'CON' : 'SIN'} bloque"
         matches = []
         result = GCFinder.api_client.execute(
-            api_method: GCFinder.google_calendar.events.list,
-            parameters: {'calendarId' => calendar_id},
-            authorization: authorization)
+          api_method: GCFinder.google_calendar.events.list,
+          parameters: {'calendarId' => calendar_id},
+          authorization: authorization)
         while true
           hashed_body = JSON.parse(result.body)
           matches.concat(hashed_body['items'].reject(&keep_if_block))
           return matches.compact if !(page_token = result.data.next_page_token)
           result = api_client.execute(
-              api_method: GCFinder.google_calendar.events.list,
-              parameters: {'calendarId' => calendar_id, 'pageToken' => page_token},
-              authorization: authorization)
+            api_method: GCFinder.google_calendar.events.list,
+            parameters: {'calendarId' => calendar_id, 'pageToken' => page_token},
+            authorization: authorization)
         end
       end
 
